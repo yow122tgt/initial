2a8276d5cc37629118de03c6ef2778f2b129551e
Nathan351 <bot56398@gmail.com>
2019-11-23 00:48:55 +0800

    20191123改

--- Calendar_test01/src/main/java/com/example/calendar_test01/Calendar_Button.java
+++ Calendar_test01/src/main/java/com/example/calendar_test01/Calendar_Button.java
@@ -22,9 +22,18 @@ public class Calendar_Button extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_calendar__button);
+
+        btn_catch_events =getSharedPreferences("cal_Events", Activity.MODE_PRIVATE);
+        for(int d=0;d< Integer.parseInt(event[0][1]);d++)
+        {
+            judge = new int[Integer.parseInt(event[0][1])];
+            judge[d]= btn_catch_events.getInt("ad"+d,0);
+        }
+
+
 /*
-        events=getSharedPreferences("Events", Activity.MODE_PRIVATE);
-        int judge_2=events.getInt("jg_2",1);
+        SharedPreferences.Editor row=events.edit();
+        row.putInt("ad"+day_3,1).commit();
 */
 
         Btn_1 = (Button) findViewById(R.id.btn);
@@ -38,11 +47,21 @@ public class Calendar_Button extends AppCompatActivity {
             public void onClick(View v) {
                 Btn_1.setBackgroundColor(Color.YELLOW);
                 calendarMain.IS確認吃藥[day_3] = 1;
+                judge[day_3] =1;
+
+                btn_to_events=getSharedPreferences("btn_Events", Activity.MODE_PRIVATE);
+                SharedPreferences.Editor row=btn_to_events.edit();
+                for(int d=0;d< Integer.parseInt(event[0][1]);d++)
+                {
+                    row.putInt("jg"+d,judge[d]).commit();
+                }
 
             }
         });
 
     }
-    public SharedPreferences events;
+    public SharedPreferences btn_catch_events;
+    public SharedPreferences btn_to_events;
+    public int judge[];
 
 }
