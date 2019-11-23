4e93624b80aa1c49cbb26a4762aecee7493c055b
Joe <unlight19950731@gmail.com>
2019-11-23 21:18:49 +0800

    20191123 介面修正(報告DEMO版)

--- app/src/main/java/medicineinfo/MedicineDetail2.java
+++ app/src/main/java/medicineinfo/MedicineDetail2.java
@@ -1,11 +1,13 @@
-package com.example.login1;
+package medicineinfo;
 
 import android.app.Activity;
 import android.os.Bundle;
 import android.widget.ImageView;
 import android.widget.TextView;
 
-public class MedicineDetail extends Activity {
+import com.example.login1.R;
+
+public class MedicineDetail2 extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,14 +17,14 @@ public class MedicineDetail extends Activity {
 
         InitialiComponent();
         txt服藥天數.setText("28");
-        txt用法用量.setText("口服 (每天2次)早晚飯後服用");
-        txt藥品名稱.setText("RIVOTRIL 0.5MG 福利全");
-        txt用途.setText("緩和焦慮相關症狀、控制癲癇發作");
-        txt副作用.setText("疲倦、肌肉無力等等");
-        detailTitle.setText("聖安健保");
-        img藥袋外觀.setImageResource(R.drawable.mb4);
+        txt用法用量.setText("口服 (每天2次)早晚飯後服用 每次1.00");
+        txt藥品名稱.setText("BEFON 5MG倍鬆釘");
+        txt用途.setText("肌肉痙攣、肌肉拉傷等症狀");
+        txt副作用.setText("鎮靜、痙攣");
+        detailTitle.setText("拉傷");
+        img藥袋外觀.setImageResource(R.drawable.mb3);
 
-      
+
     }
 
     private void InitialiComponent() {
--- app/src/main/java/medicineinfo/MedicineDetail4.java
+++ app/src/main/java/medicineinfo/MedicineDetail4.java
@@ -1,11 +1,13 @@
-package com.example.login1;
+package medicineinfo;
 
 import android.app.Activity;
 import android.os.Bundle;
 import android.widget.ImageView;
 import android.widget.TextView;
 
-public class MedicineDetail extends Activity {
+import com.example.login1.R;
+
+public class MedicineDetail4 extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -14,15 +16,18 @@ public class MedicineDetail extends Activity {
 
 
         InitialiComponent();
-        txt服藥天數.setText("28");
-        txt用法用量.setText("口服 (每天2次)早晚飯後服用");
-        txt藥品名稱.setText("RIVOTRIL 0.5MG 福利全");
-        txt用途.setText("緩和焦慮相關症狀、控制癲癇發作");
-        txt副作用.setText("疲倦、肌肉無力等等");
-        detailTitle.setText("聖安健保");
-        img藥袋外觀.setImageResource(R.drawable.mb4);
+        txt服藥天數.setText("30");
+        txt用法用量.setText("口服\n" +
+                "每一天兩次，遵照醫師指示，" +
+                "早飯後1粒，午飯後1粒");
+        txt藥品名稱.setText("Parkryl(Selegiline)[5mg/tube]\n" +
+                "中文名:巴可癒錠");
+        txt用途.setText("巴金森氏症及其他相關症狀");
+        txt副作用.setText("腸胃不適");
+        detailTitle.setText("腦神經內科");
+        img藥袋外觀.setImageResource(R.drawable.pa2);
 
-      
+
     }
 
     private void InitialiComponent() {
